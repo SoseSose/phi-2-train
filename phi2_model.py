e39fd69f096208617992b44c2ec94932c1b1f948
Sose1 <redstar0bluestar@gmail.com>
2024-03-11 22:22:06 +0900

    モデルをインスタンス化する関数を追加しinit下だけでは重みを読み込まないように

--- phi2_model.py
+++ phi2_model.py
@@ -4,53 +4,65 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-Phi2_MAX_TOKENS = 2048
-
-
 class Phi2:
-    def __init__(self, save_dir) -> None:
-        if not Path(save_dir).exists():
-            # download phi2
-            self.tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2")
-            self.model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2")
-            # save phi2
-            self.tokenizer.save_pretrained(save_dir)
-            self.model.save_pretrained(save_dir)
+    MAX_TOKENS = 2048
 
-        else:
-            self.tokenizer = AutoTokenizer.from_pretrained(
-                save_dir,
-                trust_remote_code=True,
-            )
+    def __init__(self, save_dir, device="auto"):
+        self.save_dir = save_dir
+        self.device = device
+        self.__isnt_instanced = True
 
-            self.model = AutoModelForCausalLM.from_pretrained(
-                save_dir,
-                torch_dtype="auto",
-                device_map="auto",
-                trust_remote_code=True,
-            )
+    def instancing(self):
+        if self.__isnt_instanced:
+            self.__isnt_instanced = False
+            if not Path(self.save_dir).exists():
+                # download phi2
+                self.tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2")
+                self.model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2")
+                # save phi2
+                self.tokenizer.save_pretrained(self.save_dir)
+                self.model.save_pretrained(self.save_dir)
+
+            else:
+                self.tokenizer = AutoTokenizer.from_pretrained(
+                    self.save_dir,
+                    trust_remote_code=True,
+                )
+
+                self.model = AutoModelForCausalLM.from_pretrained(
+                    self.save_dir,
+                    torch_dtype="auto",
+                    device_map=self.device,
+                    trust_remote_code=True,
+                )
+
+    def generate(self, token_ids):
+        self.instancing()
+        output_ids = self.model.generate(
+            token_ids.to(self.model.device),
+            pad_token_id=self.tokenizer.eos_token_id,
+            # ないとThe attention mask ~という警告が出る。
+            temperature=0.2,
+            do_sample=True,
+            max_length=self.MAX_TOKENS,
+        )
+        return output_ids
 
     def get_token_num_and_answer(self, question):
+        self.instancing()
         try:
             with torch.no_grad():
-                token_ids= self.tokenizer.encode(
+                token_ids = self.tokenizer.encode(
                     question,
                     add_special_tokens=True,
                     # add_special_tokens=False,
                 )
                 token_ids = torch.tensor(token_ids).unsqueeze(0)
                 token_num = len(token_ids[0])
-                if token_num > Phi2_MAX_TOKENS:
-                    answer = "token_num > {}".format(Phi2_MAX_TOKENS)
+                if token_num > self.MAX_TOKENS:
+                    answer = "token_num > {}".format(self.MAX_TOKENS)
                 else:
-                    output_ids = self.model.generate(
-                        token_ids.to(self.model.device),
-                        pad_token_id=self.tokenizer.eos_token_id,
-                        # ないとThe attention mask ~という警告が出る。
-                        temperature=0.2,
-                        do_sample=True,
-                        max_length=Phi2_MAX_TOKENS,
-                    )
+                    output_ids = self.generate(token_ids)
                     answer = self.tokenizer.decode(output_ids[0][token_ids.size(1) :])
 
         except Exception as e:
@@ -68,3 +80,6 @@ def test_get_token_num_and_anser():
     print(answer)
     assert answer is not None
     assert token_num is not None
+
+if __name__ == "__main__":
+    test_get_token_num_and_anser()
