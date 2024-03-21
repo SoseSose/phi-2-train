d79bae0b50a33cc7bcc07dea4158342ebe01a254
Sose1 <redstar0bluestar@gmail.com>
2024-03-21 22:00:39 +0900

    名前をリファクタリングし,テストを移動

--- src/const.py
+++ src/const.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from typing import Optional
 
 
+
 @dataclass
 class ArcConst:
     # COLOR: tuple[int] = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
@@ -16,17 +17,49 @@ class ArcConst:
     FAKE_NUM = 8  # FAKEありのデータセットを作ったので,1つのタスクに含まれるFAKEの数
 
 
-class Color:
+class ArcColor:
     def __init__(self):
-        self.color_cand = [
+        self.cand = [
             i for i in range(ArcConst.MIN_COLOR_NUM, ArcConst.MAX_COLOR_NUM)
         ]
 
     def pick_random_unused(self, index: Optional[int] = None) -> int:
         if index is not None:
-            if index < 0 or index >= len(self.color_cand):
-                raise Exception(f"index must be 0 <= index < {len(self.color_cand)}")
-            picked = self.color_cand.pop(index)
+            if index < 0 or index >= len(self.cand):
+                raise Exception(f"index must be 0 <= index < {len(self.cand)}")
+            picked = self.cand.pop(index)
         else:
-            picked = self.color_cand.pop(random.randint(0, len(self.color_cand) - 1))
+            picked = self.cand.pop(random.randint(0, len(self.cand) - 1))
         return picked
+
+import pytest
+
+def test_color_pick():
+    for _ in range(1000):
+        color = ArcColor()
+        first_picked_color = color.pick_random_unused(index=None)
+        second_picked_color = color.pick_random_unused(index=None)
+        assert first_picked_color != second_picked_color
+
+
+def test_all_color_pick():
+    color = ArcColor()
+    for i in range(ArcConst.MAX_COLOR_NUM):
+        picked_color = color.pick_random_unused(index=0)
+        assert picked_color == i
+
+    # it should be error
+    color = ArcColor()
+    with pytest.raises(Exception) as e:
+        picked_color = color.pick_random_unused(index=-1)
+    assert str(e.value) == f"index must be 0 <= index < {ArcConst.MAX_COLOR_NUM}"
+
+    # it should not be
+    color = ArcColor()
+    picked_color = color.pick_random_unused(index=ArcConst.MAX_COLOR_NUM - 1)
+
+    # it should be error
+    color = ArcColor()
+    with pytest.raises(Exception) as e:
+        picked_color = color.pick_random_unused(index=ArcConst.MAX_COLOR_NUM)
+    assert str(e.value) == f"index must be 0 <= index < {ArcConst.MAX_COLOR_NUM}"
