d2b10d3bdc255cfda6dc2bdb112630dfde77a126
Sose1 <redstar0bluestar@gmail.com>
2024-03-27 21:17:05 +0900

    データ用のconfig,mlflow用のconfigを追加

--- src/config/train_config.py
+++ src/config/train_config.py
@@ -8,6 +8,8 @@ class TrainConfig:
     pretrained_model: str = "roberta-base"
     num_classes: int = 2
     lr: float = 2e-4
+    device = "cuda:0"
+
     max_length: int = 128
     batch_size: int = 256
     num_workers: int = os.cpu_count()
@@ -20,11 +22,21 @@ class TrainConfig:
     )
     min_delta: float = 0.005
     patience: int = 4
-    
+
+@dataclass
+class DataModuleConfig:
+    data_dir: str = "data/logical_op"
+    train_num: int = 1000
+    eval_num: int = 200
+    task_len: int = 5
+    batch_size: int = 1
+
+@dataclass
+class MLFLowConfig: 
 
     # MLflow
-    mlflow_experiment_name: str = "Lex-GLUE Terms of Service"
-    mlflow_run_name: str = "onnx-gpu-run12"
-    mlflow_description: str = (
-        f"PEFT tune roberta-base to classify {mlflow_experiment_name}."
-    )
+    mlflow_tracking_uri: str = "result/mlruns.db"
+    mlflow_artifact_location: str = "result/artifacts"
+    mlflow_experiment_name: str = "phi2-train-logical-op"
+    mlflow_run_name: str = "first-run"
+    mlflow_description: str =  "オリジナルのPHI2から論理演算の学習をさせる"
